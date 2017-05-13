790feddcdbbdd1f32ecce00037403ccee277542a
David Wiedenmann <david.wiedenmann@gmail.com>
2017-05-12 18:36:40 -0700

    Hash Tables: Ransom Note

--- Cracking The Coding Interview/Data Structures/003 Hash Tables: Ransom Note/Solution.java
+++ Cracking The Coding Interview/Data Structures/003 Hash Tables: Ransom Note/Solution.java
@@ -17,7 +17,8 @@
 *		He found a magazine and wants to know if he can cut out whole words from it
 *		and use them to create an untraceable replica of his ransom note. The words
 *		in his note are case-sensitive and he must use whole words available in the
-*		magazine, meaning he cannot use substrings or concatenation to create the words he needs.
+*		magazine, meaning he cannot use substrings or concatenation to create the
+*		words he needs.
 *
 *		Given the words in the magazine and the words in the ransom note, print Yes
 *		if he can replicate his ransom note exactly using whole words from the
@@ -45,7 +46,6 @@
 *			of his ransom note; otherwise, print No.
 *
 *		Sample Input
-*
 *			6 4
 *			give me one grand today night
 *			give one grand today
@@ -73,6 +73,51 @@ import java.util.*;
 
 
 public class Solution {
+	Map<String, Integer> magazineMap;
+	Map<String, Integer> noteMap;
+
+	public Solution(String magazine, String note) {
+		// Initialize Maps
+		this.magazineMap = new Hashtable<String, Integer>();
+		this.noteMap = new Hashtable<String, Integer>();
+
+		// Create magazine map
+		String[] magazineWords 	= magazine.split(" ");
+		String[] noteWords 			= note.split(" ");
+		for (int i = 0; i < magazineWords.length; i++) {
+			//magazineMap.put(magazineWords[i], 1);
+			String w = magazineWords[i];
+			int n;
+			if (magazineMap.containsKey(w))
+				n = magazineMap.get(w);
+			else n = 0;
+			magazineMap.put(w, n + 1);
+		}
+
+		// Create note map
+		for (int i = 0; i < noteWords.length; i++) {
+			//noteMap.put(noteWords[i], 1);
+			String w = noteWords[i];
+			int n;
+			if (noteMap.containsKey(w))
+				n = noteMap.get(w);
+			else n = 0;
+			noteMap.put(w, n + 1);
+		}
+	}
+
+	public boolean solve() {
+		for (String key : noteMap.keySet()){
+			// if the map doesn't contain this word at all, return false
+			if (!magazineMap.containsKey(key)) return false;
+
+			// compare the counts, if magazine has less occurrences than the note fail
+			int n = noteMap.get(key);
+			int m = magazineMap.get(key);
+			if (m < n) return false;
+		}
+		return true;
+	}
 
 // Main
 public static void main(String[] args) {
@@ -85,7 +130,7 @@ public static void main(String[] args) {
 	if ( debug == true ) {
 		try {
 			String workingDir = new File(System.getProperty("user.dir")).getParent();
-			String projectDir = "002 Strings: Making Anagrams";
+			String projectDir = "003 Hash Tables: Ransom Note";
 			String inputFile = workingDir + "/" + projectDir + "/input1.txt";
 
 			System.setIn(new FileInputStream( new File(inputFile) ));
@@ -94,62 +139,22 @@ public static void main(String[] args) {
 			System.out.println(e);
 		}
 	}
-	// String outputFile = debugPath + "output.txt";
-	//try {  }
-	//catch (IOException e) { System.out.println("InputFile not found"); }
-	//}
+	Scanner scanner = new Scanner(System.in);
+	int m = scanner.nextInt();
+	int n = scanner.nextInt();
 
-	// Scan Input
-	Scanner sc = new Scanner(System.in);
+	// Eat whitespace to beginning of next line
+	scanner.nextLine();
+	String magazine = scanner.nextLine();
+	String note = scanner.nextLine();
+	Solution s = new Solution(magazine, note);
+	scanner.close();
 
-	// scan the strings
-	String a = sc.nextLine();
-	String b = sc.nextLine();
-	System.out.println(numberNeeded(a, b));
-
-	// Close the Scanner
-	sc.close();
-}
-
-private static int numberNeeded(String a, String b){
-	// handle large k
-	//if (k >= n)
-	//	k = k % n;
-
-	//System.out.println(a);
-	//System.out.println(b);
-
-	Hashtable<Character, Integer> hash = new Hashtable<Character, Integer>();
-
-	char c = '\u0000';
-	int n;
-
-	// Count characters in string a
-	for ( int i = 0; i < a.length(); i++ ) {
-		c = a.charAt(i);
-		if (hash.containsKey(c))
-			n = hash.get(c);
-		else n = 0;
-		hash.put(c, n + 1);
-	}
-
-	// Count characters in string b
-	for ( int i = 0; i < b.length(); i++ ) {
-		c = b.charAt(i);
-		//System.out.println(c);
-		if (hash.containsKey(c))
-			n = hash.get(c);
-		else n = 0;
-		hash.put(c, n - 1);
-	}
-
-	int d = 0;
-	for(Character key: hash.keySet()) {
-		d += Math.abs(hash.get(key));
-		//System.out.format("%s: %d\n", key, hash.get(key));
-	}
-
-	return d;
+	boolean answer = s.solve();
+	if(answer)
+			System.out.println("Yes");
+	else System.out.println("No");
 
 }
+
 }
